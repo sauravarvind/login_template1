69e1cbaaf6cb4197ac131198802aa98623f16374
Saurav Arvind <66073647+sauravarvind@users.noreply.github.com>
2021-09-04 17:00:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+var attempt = 3; // Variable to count number of attempts.
+// Below function Executes on click of login button.
+function validate(){
+var username = document.getElementById("username").value;
+var password = document.getElementById("password").value;
+if ( username == "sauravarvind" && password == "saurav#123"){
+//window.location = "success.html";
+window.open('success.html');
+return false;
+}
+else{
+attempt --;
+alert("You have left "+attempt+" attempt;");
+if( attempt == 0){
+document.getElementById("username").disabled = true;
+document.getElementById("password").disabled = true;
+document.getElementById("submit").disabled = true;
+return false;
+}
+}
+}
